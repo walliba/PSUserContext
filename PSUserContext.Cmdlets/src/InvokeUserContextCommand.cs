31d5667ba30293383a3e63894667020f497938fa
walliba <bryce.wallis@compsys.com>
2026-01-08 16:56:50 -0600

    style: code format

--- PSUserContext.Cmdlets/src/InvokeUserContextCommand.cs
+++ PSUserContext.Cmdlets/src/InvokeUserContextCommand.cs
@@ -10,28 +10,27 @@ using PSUserContext.Cmdlets.Helpers;
 
 namespace PSUserContext.Cmdlets;
 
-[Cmdlet(VerbsLifecycle.Invoke, "UserContext", DefaultParameterSetName = "ById+ScriptBlock", SupportsShouldProcess = true)]
+[Cmdlet(VerbsLifecycle.Invoke, "UserContext", DefaultParameterSetName = "ById+ScriptBlock",
+    SupportsShouldProcess = true)]
 [OutputType(typeof(UserProcessResult))]
 [OutputType(typeof(UserProcessWithOutputResult))]
 public sealed class InvokeUserContextCommand : PSCmdlet
 {
-    private const string ById        = "ById";
-    private const string ByConsole   = "ByConsole";
-    private const string CommandAct  = "+ScriptBlock";
-    private const string FileAct     = "+File";
-    private const string RedirectAct = "+Redirect";
-    private const string VisibleAct  = "+Visible";
-    
-    private const string ByIdCommand   = ById + CommandAct;
-    private const string ByIdFile      = ById + FileAct;
-    
+    private const string ById             = "ById";
+    private const string ByConsole        = "ByConsole";
+    private const string CommandAct       = "+ScriptBlock";
+    private const string FileAct          = "+File";
+    private const string ByIdCommand      = ById + CommandAct;
+    private const string ByIdFile         = ById + FileAct;
     private const string ByConsoleCommand = ByConsole + CommandAct;
-    private const string ByConsoleFile = ByConsole + FileAct;
-    
-    [Parameter(Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ByIdCommand)]
-    [Parameter(Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ByIdFile)]
-    public uint SessionId { get; set; } = SessionExtensions.INVALID_SESSION_ID;
-    
+    private const string ByConsoleFile    = ByConsole + FileAct;
+
+    [Parameter(Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true,
+        ParameterSetName = ByIdCommand)]
+    [Parameter(Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true,
+        ParameterSetName = ByIdFile)]
+    public uint SessionId { get; set; }
+
     /// <summary>
     /// When specified, invokes the active console session.
     /// </summary>
@@ -41,51 +40,51 @@ public sealed class InvokeUserContextCommand : PSCmdlet
     [Parameter(Mandatory = true, ParameterSetName = ByConsoleCommand)]
     [Parameter(Mandatory = true, ParameterSetName = ByConsoleFile)]
     public SwitchParameter Console { get; set; }
-    
+
     [Parameter(Mandatory = true, Position = 0, ParameterSetName = ByIdCommand)]
     [Parameter(Mandatory = true, Position = 0, ParameterSetName = ByConsoleCommand)]
-    public ScriptBlock? ScriptBlock { get; set; }
-    
+    public ScriptBlock ScriptBlock { get; set; }
+
     [Parameter(Mandatory = true, ParameterSetName = ByIdFile)]
     [Parameter(Mandatory = true, ParameterSetName = ByConsoleFile)]
     [Alias("File")]
     [ArgumentCompleter(typeof(ScriptFileCompleter))]
+    // todo: fix file input
+    // ref: https://stackoverflow.com/questions/8505294/how-do-i-deal-with-paths-when-writing-a-powershell-cmdlet
     public FileInfo? FilePath { get; set; }
-    
+
     [Parameter(Position = 2)]
     [Alias("Args")]
     public string[] Arguments { get; set; } = Array.Empty<string>();
-    
+
     // Attempting to specify -RedirectOutput together with -ShowWindow will result in a parameter binding error.
-    [Parameter] 
-    [Alias("Out")] 
-    public SwitchParameter RedirectOutput { get; set; }
-    
+    [Parameter] [Alias("Out")] public SwitchParameter RedirectOutput { get; set; }
+
     // todo: implement PassThru parameter
-    
-    [Parameter]
-    [Alias("Visible")]
-    public SwitchParameter ShowWindow { get; set; }
+
+    [Parameter] [Alias("Visible")] public SwitchParameter ShowWindow { get; set; }
 
     private const string RequiredPrivilege = "SeDelegateSessionUserImpersonatePrivilege";
     private const string PowerShellPath    = @"C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe";
-    
+
     private PropertyInfo? _propertyInfo;
-    
+
     protected override void BeginProcessing()
-    {   
+    {
         // temporary solution until I source generate parameter sets
         if (ShowWindow.IsPresent && RedirectOutput.IsPresent)
             throw new ParameterBindingException(
                 "The parameters -ShowWindow and -RedirectOutput cannot be used together. This is a Win32 limitation.");
-        
+
         // todo: fix broken api
         // if (!TokenExtensions.HasTokenPrivilege(RequiredPrivilege))
         //     throw new InvalidOperationException(
         //         "Missing required privilege. You must run this script as SYSTEM or have the SeDelegateSessionUserImpersonatePrivilege token.");
-        
-        _propertyInfo = typeof(ErrorRecord).GetProperty("PreserveInvocationInfoOnce", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        _propertyInfo = typeof(ErrorRecord).GetProperty("PreserveInvocationInfoOnce",
+            BindingFlags.NonPublic | BindingFlags.Instance);
     }
+
     protected override void ProcessRecord()
     {
         if (!ShouldProcess("ScriptBlock")) return;
@@ -93,12 +92,12 @@ public sealed class InvokeUserContextCommand : PSCmdlet
         StringBuilder sbCommand =
             new StringBuilder(
                 $"\"{PowerShellPath}\" -ExecutionPolicy Bypass -NoLogo -OutputFormat XML -WindowStyle {(ShowWindow ? "Normal" : "Hidden")}");
-        
+
         if (ParameterSetName.Equals(ByIdFile))
         {
             // Should probably copy to session's temp, ensuring the user has access to the file.
             sbCommand.Append($" -File \"{FilePath.FullName}\"");
-            
+
             if (MyInvocation.BoundParameters.ContainsKey("Arguments"))
             {
                 string arguments = string.Join(" ", Arguments);
@@ -109,20 +108,20 @@ public sealed class InvokeUserContextCommand : PSCmdlet
         {
             string encodedCommand = Convert.ToBase64String(Encoding.Unicode.GetBytes(ScriptBlock!.ToString()));
             sbCommand.Append($" -EncodedCommand {encodedCommand}");
-            
+
             if (MyInvocation.BoundParameters.ContainsKey("Arguments"))
             {
                 string arguments =
                     Convert.ToBase64String(
                         Encoding.Unicode.GetBytes(
                             PSSerializer.Serialize(Arguments))
-                            );
+                    );
                 sbCommand.Append($" -EncodedArguments {arguments}");
             }
         }
 
         SafeFileHandle primaryToken;
-        
+
         if (Console.IsPresent)
         {
             var consoleId = SessionExtensions.GetActiveConsoleSession();
@@ -156,21 +155,21 @@ public sealed class InvokeUserContextCommand : PSCmdlet
                     Redirect = redirectOptions,
                     WindowStyle = (ushort)(ShowWindow ? 5 : 0)
                 });
-            
+
             var output = CliXml.Deserialize(result.StdOutput);
             var err = CliXml.DeserializeError(result.StdError);
-            
+
             if (output is not null)
                 foreach (var o in output)
                     WriteObject(o);
-            
+
             if (err is not null)
                 foreach (var o in err)
                 {
                     _propertyInfo?.SetValue(o, true);
                     WriteError(o);
                 }
-            
+
             if (RedirectOutput.IsPresent)
                 WriteObject(new UserProcessWithOutputResult
                 {
@@ -187,16 +186,15 @@ public sealed class InvokeUserContextCommand : PSCmdlet
             //         SessionId = SessionId,
             //         ExitCode = result.ExitCode,
             //     });
-            
         }
     }
 }
 
 public class UserProcessResult
 {
-    public uint   ProcessId   { get; set; }
-    public uint   SessionId   { get; set; }
-    public uint    ExitCode    { get; set; }
+    public uint ProcessId { get; set; }
+    public uint SessionId { get; set; }
+    public uint ExitCode  { get; set; }
 
     public override string ToString()
     {
